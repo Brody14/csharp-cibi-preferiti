1ee9523f5a701ecfd35da5c5c7a2e318c579cd7b
Brody14 <stefania.spinas@gmail.com>
2023-09-05 15:50:58 +0200

    Esercizio completo

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] cibiPreferiti = { "Pistacchi in ogni loro forma", "Pizza", "Gazpacho", "Tiramisù", "Hummus" };
+
+Console.WriteLine(cibiPreferiti.Length);
+
+for(var i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(i+1 + " " + cibiPreferiti[i] + " ");
+}
+
+Console.WriteLine("Il cibo preferito è: " + cibiPreferiti[0]);
+Console.WriteLine("Il cibo meno preferito è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
+
+
+
