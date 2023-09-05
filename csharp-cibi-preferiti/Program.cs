883e3e2c864ce3c18e0d6bef9f7b21690d8d26d7
Brody14 <stefania.spinas@gmail.com>
2023-09-05 16:03:08 +0200

    Esercizio completato senza bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine(cibiPreferiti.Length);
 
 for(var i = 0; i < cibiPreferiti.Length; i++)
 {
-    Console.WriteLine(i+1 + " " + cibiPreferiti[i] + " ");
+    Console.WriteLine($"{ i + 1} {cibiPreferiti[i]}");
 }
 
-Console.WriteLine("Il cibo preferito è: " + cibiPreferiti[0]);
-Console.WriteLine("Il cibo meno preferito è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
+Console.WriteLine($"Il cibo preferito è: {cibiPreferiti[0]}");
+Console.WriteLine($"Il cibo meno preferito è: {cibiPreferiti[cibiPreferiti.Length - 1]}");
 
 
 
