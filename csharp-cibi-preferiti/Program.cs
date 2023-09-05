07140b2887549ca4f40a285a6168f1278463b143
Brody14 <stefania.spinas@gmail.com>
2023-09-05 17:10:57 +0200

    Edit

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -3,13 +3,34 @@ string[] cibiPreferiti = { "Pistacchi in ogni loro forma", "Pizza", "Gazpacho", 
 
 Console.WriteLine(cibiPreferiti.Length);
 
-for(var i = 0; i < cibiPreferiti.Length; i++)
+for(int i = 0; i < cibiPreferiti.Length; i++)
 {
     Console.WriteLine($"{ i + 1} {cibiPreferiti[i]}");
 }
 
 Console.WriteLine($"Il cibo preferito è: {cibiPreferiti[0]}");
 Console.WriteLine($"Il cibo meno preferito è: {cibiPreferiti[cibiPreferiti.Length - 1]}");
+
+string median;
+
+if (cibiPreferiti.Length % 2 == 0)
+{
+    int length = cibiPreferiti.Length / 2;
+    median = ( cibiPreferiti[length - 1] + " " + cibiPreferiti[length] );
+}else
+{
+    int length = cibiPreferiti.Length / 2;
+    median = cibiPreferiti[length];
+
+}
+
+Console.WriteLine($"La mediana è: {median}");
+
+
+
+
+
+
 
 
 
